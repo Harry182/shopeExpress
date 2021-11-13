3569e5d1ad09a218f142c4e1249895f11fd16257
@gopdev <markus44p@gmail.com>
2021-11-12 23:15:50 -0600

    nuestro primer endpoint GET y parámetros

--- index.js
+++ index.js
@@ -7,6 +7,10 @@ const port = process.env.PORT || 3000;
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
+app.get("/hola/:name", (req, res) => {
+  res.send({ message: `${req.params.name}` });
+});
+
 app.listen(port, () => {
   console.log(`Api rest corriendo en htpp://localhost:${port}`);
 });
